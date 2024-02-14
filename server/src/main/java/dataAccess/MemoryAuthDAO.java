3a00794bfb5aa821aacacdb350bb0786957de6e0
Dallin Weaver <dallin.j.weaver@gmail.com>
2024-02-13 22:17:23 -0700

    Implemented the AuthDAO interface (memory only)

---
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -0,0 +1,45 @@
+package dataAccess;
+import model.AuthData;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private static MemoryAuthDAO INSTANCE;
+    private ArrayList<AuthData> authDatabase;
+
+    private MemoryAuthDAO() {
+        authDatabase = new ArrayList<>();
+    }
+
+    public static MemoryAuthDAO getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new MemoryAuthDAO();
+        }
+        return INSTANCE;
+    }
+
+    public void createAuth(AuthData a) throws DataAccessException {
+        authDatabase.add(a);
+    }
+    public AuthData readUser(String username) throws DataAccessException {
+        Iterator<AuthData> iter =  authDatabase.iterator();
+        while(iter.hasNext()) {
+            AuthData next = iter.next();
+            if (Objects.equals(next.username(), username)) return next;
+        }
+        return null;
+    }
+    public AuthData readAuth(String authToken) throws DataAccessException {
+        Iterator<AuthData> iter =  authDatabase.iterator();
+        while(iter.hasNext()) {
+            AuthData next = iter.next();
+            if (Objects.equals(next.authToken(), authToken)) return next;
+        }
+        return null;
+    }
+    public void delete(AuthData a) throws DataAccessException {
+        authDatabase.remove(a);
+    }
+}
