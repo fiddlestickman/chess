78dd887e39b1838780df8fd2b0d192f0438ba7cf
Dallin Weaver <dallin.j.weaver@gmail.com>
2024-03-21 18:52:35 -0600

    included code for joining a game

--- client/src/main/java/PregameMenu.java
+++ client/src/main/java/PregameMenu.java
@@ -1,8 +1,11 @@
+import chess.ChessGame;
 import model.CreateGameData;
 import model.GameData;
+import model.JoinGameData;
 import model.LoginData;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -35,10 +38,47 @@ public class PregameMenu {
             return "keep looping";
         }
         else if (Objects.equals(input, "3") || Objects.equals(input, "list games")) {
+            ArrayList<GameData> games = listGames();
+            if (games != null) {
+                Iterator<GameData> gameiter = games.iterator();
+                System.out.print("ID - Game Name - White Username - Black Username");
+                while (gameiter.hasNext()) {
+                    GameData next = gameiter.next();
+                    String white = next.whiteUsername();
+                    String black = next.blackUsername();
+                    if (next.whiteUsername() == null || next.whiteUsername().isEmpty()) {
+                        white = "null";
+                    }
+                    if (next.blackUsername() == null || next.blackUsername().isEmpty()) {
+                        black = "null";
+                    }
+                    System.out.printf("%d - %s - %s - %s%n", next.gameID(), next.gameName(), white, black);
+                }
+            }
+            else {
+                System.out.print("No games found");
+            }
 
         }
         else if (Objects.equals(input, "4") || Objects.equals(input, "join game")) {
+            String id = getString("Please enter a game ID: ");
+            String team = getString("Please enter a team color to play as (white/black/none): ");
+            ChessGame.TeamColor color = null;
+            if (team.equals("white") || team.equals("w")){
+                color = ChessGame.TeamColor.WHITE;
+            } else if (team.equals("black") || team.equals("b"))
+                color = ChessGame.TeamColor.BLACK;
+            int gameID = Integer.parseInt(id);
+            Main.JoinResponse out = joinGame(gameID, color);
+            if (out != null && out.playerColor != null) {
+                System.out.printf("Joined game on the %s team.%n", out.playerColor);
+            } else if (out != null){
+                System.out.print("Joined game as an observer\n");
+            } else {
+                //error handling
+            }
 
+            return "keep looping";
         }
         else if (Objects.equals(input, "5") || Objects.equals(input, "join observer")) {
 
@@ -67,13 +107,25 @@ public class PregameMenu {
 
     private ArrayList<GameData> listGames() {
         HTTPHandler handler = new HTTPHandler(auth, url + "/game");
-
         try {
             Main.ListResponse games = (Main.ListResponse) handler.Request("GET", null, Main.ListResponse.class);
             return games.games;
         } catch (Exception e) {
             return null;
         }
+    }
+
+    private Main.JoinResponse joinGame(int gameID, ChessGame.TeamColor color) {
+        HTTPHandler handler = new HTTPHandler(auth, url + "/game");
+        JoinGameData data = new JoinGameData(color, gameID);
+        handler.serialize(data);
+        try {
+            Main.JoinResponse response = (Main.JoinResponse) handler.Request("PUT", data, Main.JoinResponse.class);
+            return response;
+        } catch (Exception e) {
+            return null;
+        }
+
     }
 
     private String getString(String prompt) {
