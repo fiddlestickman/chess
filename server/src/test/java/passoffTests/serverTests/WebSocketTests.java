66a79ad50aecad5e7f137a8aa106439f530f3120
Dallin Weaver <dallin.j.weaver@gmail.com>
2024-03-28 12:35:21 -0600

    Copied TestFactory to the server tests folder, fixing bugs

--- server/src/test/java/passoffTests/serverTests/WebSocketTests.java
+++ server/src/test/java/passoffTests/serverTests/WebSocketTests.java
@@ -4,7 +4,6 @@ import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPosition;
 import org.junit.jupiter.api.*;
-import passoffTests.TestFactory;
 import passoffTests.obfuscatedTestClasses.TestServerFacade;
 import passoffTests.testClasses.TestException;
 import passoffTests.testClasses.TestModels;
