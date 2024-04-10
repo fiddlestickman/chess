5756df92ed0d0e4ce788cd8f333dfa364bb54ba1
Dallin Weaver <dallin.j.weaver@gmail.com>
2024-04-10 11:19:56 -0600

    wrote code for handling join player and join observer commands

--- server/src/main/java/websocket/WSManager.java
+++ server/src/main/java/websocket/WSManager.java
@@ -1,8 +1,11 @@
 package websocket;
 
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dataAccess.*;
+import model.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.*;
 import server.Handler;
@@ -11,17 +14,134 @@ import spark.Spark;
 import webSocketMessages.serverMessages.*;
 import webSocketMessages.userCommands.*;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WSManager {
 
-    private ConcurrentHashMap<String, WSManager.Connection> connections;
 
     public WSManager () {
-        this.connections = new ConcurrentHashMap<>();
     }
+
+    public NotificationMessage joinPlayerNotify(JoinPlayerCommand command) throws DataAccessException {
+        int gameID = command.getGameID();
+        ChessGame.TeamColor color = command.getColor();
+        String auth = command.getAuthString();
+
+        AuthDAO authDAO = SQLAuthDAO.getInstance();
+        GameDAO gameDAO = SQLGameDAO.getInstance();
+
+        AuthData authdata = authDAO.readAuth(auth);
+        if(authdata == null) {
+            throw new DataAccessException("Authtoken was wrong");
+        }
+
+        GameData gamedata = gameDAO.readGameID(gameID);
+        String strcolor = null;
+
+        if (color == ChessGame.TeamColor.WHITE) {
+            strcolor = "white";
+            if (!Objects.equals(gamedata.whiteUsername(), authdata.username())) {
+                throw new DataAccessException("authtoken didn't match white player");
+            }
+        } else if (color == ChessGame.TeamColor.BLACK) {
+            strcolor = "black";
+            if (!Objects.equals(gamedata.blackUsername(), authdata.username())) {
+                throw new DataAccessException("authtoken didn't match black player");
+            }
+        } else {
+            throw new DataAccessException("No color in join player command");
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Player ");
+        builder.append(authdata.username());
+        builder.append(" joining on the ");
+        builder.append(strcolor);
+        builder.append("team\n");
+
+        String join = builder.toString();
+
+        return new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, join);
+    }
+
+    public NotificationMessage joinObserverNotify(JoinObserverCommand command) throws DataAccessException {
+        int gameID = command.getGameID();
+        String auth = command.getAuthString();
+
+        AuthDAO authDAO = SQLAuthDAO.getInstance();
+        WatchDAO watchDAO = SQLWatchDAO.getInstance();
+
+        AuthData authdata = authDAO.readAuth(auth);
+        if(authdata == null) {
+            throw new DataAccessException("Authtoken was wrong");
+        }
+
+        WatchData watch = watchDAO.findWatch(authdata.username(), gameID);
+
+        if (watch == null) {
+            throw new DataAccessException("No watcher with that authtoken found");
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Observer ");
+        builder.append(authdata.username());
+        builder.append(" has joined\n");
+
+        String join = builder.toString();
+
+        return new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, join);
+    }
+
+    public LoadGameMessage loadGame(JoinPlayerCommand command) throws DataAccessException {
+        int gameID = command.getGameID();
+        GameDAO gameDAO = SQLGameDAO.getInstance();
+        GameData gamedata = gameDAO.readGameID(gameID);
+
+        if (gamedata == null) {
+            throw new DataAccessException("No game found with given gameID");
+        }
+        return new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, gamedata.game());
+    }
+    public LoadGameMessage loadGame(JoinObserverCommand command) throws DataAccessException {
+        int gameID = command.getGameID();
+        GameDAO gameDAO = SQLGameDAO.getInstance();
+        GameData gamedata = gameDAO.readGameID(gameID);
+
+        if (gamedata == null) {
+            throw new DataAccessException("No game found with given gameID");
+        }
+        return new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, gamedata.game());
+    }
+
+
+    public LoadGameMessage makeMove() {
+        //server verifies validity of the move
+        //updates the game to represent the move
+        //send a load_game message to all clients
+        //send a notification to all other clients
+        return null;
+    }
+
+    public NotificationMessage leave() {
+        //update the game to remove root client
+        //send a notification to all other clients
+        return null;
+    }
+
+    public LoadGameMessage resign() {
+        //update game as over in the database
+        //send a notification to all other clients
+        return null;
+    }
+    public NotificationMessage resignMessage() {
+        return null;
+    }
+
     //have a list of all the connections that matter
     //pair connections with usernames somehow
     //when you try to broadcast, look up with DAOs the relevant users
@@ -30,108 +150,4 @@ public class WSManager {
     //and game state
     //make sure game moves are legal
 
-    public void add(String username, Session session) {
-        Connection connection = new Connection(username, session);
-        connections.put(username, connection);
-    }
-
-    public void remove(String username) {
-        connections.remove(username);
-    }
-
-    public void loadGame(int gameID, LoadGameMessage message) {
-        //send the current game state to each client
-
-        ArrayList<String> users;
-        WSService service = new WSService();
-        try {
-            users = service.getUsers(gameID); //these are all the users, both watching and playing
-        } catch (Exception e) {
-            //error handling
-            return;
-        }
-
-        Iterator<String> iter = users.iterator();
-
-        while(iter.hasNext()) {
-            String next = iter.next();
-            Connection connection = connections.get(next);
-            try {
-                if (connection.getSession().isOpen()) {
-                    connection.getSession().getRemote().sendString(serialize(message));
-                } else {
-                    service.delete(next, gameID);
-                }
-            } catch (Exception e) {
-                //error handling
-            }
-        }
-
-    }
-    public void notify(int gameID, String host, NotificationMessage message) {
-
-        ArrayList<String> users;
-        WSService service = new WSService();
-        try {
-            users = service.getUsers(gameID); //these are all the users, both watching and playing
-        } catch (Exception e) {
-            //error handling
-            return;
-        }
-
-        Iterator<String> iter = users.iterator();
-
-        while(iter.hasNext()) {
-            String next = iter.next();
-            Connection connection = connections.get(next);
-            if (!next.equals(host)) {
-                try {
-                    if (connection.getSession().isOpen()) {
-                        connection.getSession().getRemote().sendString(serialize(message));
-                    } else {
-                        service.delete(next, gameID);
-                    }
-                } catch (Exception e) {
-                    //error handling
-                }
-            }
-        }
-        //session.getRemote().sendString("WebSocket response: " + message);
-    }
-
-    public void errorBroadcast(String host, ErrorMessage message) {
-
-        Connection connection = connections.get(host);
-        try {
-            if (connection.getSession().isOpen()) {
-                connection.getSession().getRemote().sendString(serialize(message));
-            }
-        } catch (Exception e) {
-            //error handling
-        }
-    }
-
-    private class Connection {
-        private String username;
-        private Session session;
-
-        Connection(String username, Session session) {
-            this.username = username;
-            this.session = session;
-        }
-
-        String getUsername() {
-            return username;
-        }
-        Session getSession() {
-            return session;
-        }
-    }
-
-    public String serialize (Object thing) {
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        return gson.toJson(thing);
-    }
 }
--- server/src/main/java/websocket/WSServer.java
+++ server/src/main/java/websocket/WSServer.java
@@ -1,19 +1,25 @@
 package websocket;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.*;
 import server.GameHandler;
 import server.Handler;
 import server.UserHandler;
+import service.WSService;
 import spark.Spark;
 import webSocketMessages.serverMessages.*;
 import webSocketMessages.userCommands.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
 @WebSocket
 public class WSServer {
-    private final WSManager connections = new WSManager();
+
+    private ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();;
 
     @OnWebSocketConnect
     public void onConnect(Session session) throws Exception {
@@ -28,8 +34,11 @@ public class WSServer {
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws Exception {
         Handler handler = new Handler();
+        WSManager manager = new WSManager();
+
         UserGameCommand command = (UserGameCommand) handler.deserialize(message, UserGameCommand.class);
         if (command.getCommandType() == UserGameCommand.CommandType.JOIN_PLAYER) {
+
 
 
             JoinPlayerCommand temp = (JoinPlayerCommand) command;
@@ -61,6 +70,93 @@ public class WSServer {
         //you can use session.getRemote().sendString(String str) to send a serialized response
     }
 
+    public void add(String username, Session session) {
+        Connection connection = new Connection(username, session);
+        connections.put(username, connection);
+    }
+
+    public void remove(String username) {
+        connections.remove(username);
+    }
+
+    public void loadGame(int gameID, LoadGameMessage message) {
+        //send the current game state to each client
+
+        ArrayList<String> users;
+        WSService service = new WSService();
+        try {
+            users = service.getUsers(gameID); //these are all the users, both watching and playing
+        } catch (Exception e) {
+            //error handling
+            return;
+        }
+
+        Iterator<String> iter = users.iterator();
+
+        while(iter.hasNext()) {
+            String next = iter.next();
+            Connection connection = connections.get(next);
+            try {
+                if (connection.getSession().isOpen()) {
+                    connection.getSession().getRemote().sendString(serialize(message));
+                } else {
+                    service.delete(next, gameID);
+                }
+            } catch (Exception e) {
+                //error handling
+            }
+        }
+
+    }
+    public void notify(int gameID, String host, NotificationMessage message) {
+
+        ArrayList<String> users;
+        WSService service = new WSService();
+        try {
+            users = service.getUsers(gameID); //these are all the users, both watching and playing
+        } catch (Exception e) {
+            //error handling
+            return;
+        }
+
+        Iterator<String> iter = users.iterator();
+
+        while(iter.hasNext()) {
+            String next = iter.next();
+            Connection connection = connections.get(next);
+            if (!next.equals(host)) {
+                try {
+                    if (connection.getSession().isOpen()) {
+                        connection.getSession().getRemote().sendString(serialize(message));
+                    } else {
+                        service.delete(next, gameID);
+                    }
+                } catch (Exception e) {
+                    //error handling
+                }
+            }
+        }
+    }
+
+    public void errorBroadcast(String host, ErrorMessage message) {
+        //only sends an error to the user that made the command
+        Connection connection = connections.get(host);
+        try {
+            if (connection.getSession().isOpen()) {
+                connection.getSession().getRemote().sendString(serialize(message));
+            }
+        } catch (Exception e) {
+            //error handling
+        }
+    }
+
+    public String serialize (Object thing) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        return gson.toJson(thing);
+    }
+
     private class Connection {
         private String username;
         private Session session;
@@ -69,7 +165,6 @@ public class WSServer {
             this.username = username;
             this.session = session;
         }
-
         String getUsername() {
             return username;
         }
