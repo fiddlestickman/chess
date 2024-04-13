a0721fc984ed2aecbef385154c4268857a074953
Dallin Weaver <dallin.j.weaver@gmail.com>
2024-04-12 20:05:24 -0600

    deleted some unused code

--- client/src/main/java/client/GameplayMenu.java
+++ client/src/main/java/client/GameplayMenu.java
@@ -267,32 +267,3 @@ public class GameplayMenu extends Endpoint {
         return new ChessMove(start, end, promo);
     }
 }
-
-
-
-
-/*
-when you join as a player or observer, open a websocket connection
-use the new connection to join
-load up the UI for the joined player
-wait for client response
-
-client can send join (two types), make move, leave, and resign commands
-join is only used to connect players to games
-make move tells the server which move they want to make
-leave tells the server they don't want to play anymore (others can join)
-resign tells the server that your side loses
-
-server can send load game, error, and notification messages
-load game is used when someone makes a move
-error for some kind of mistake
-notification for things like players joining, making a move, being in check or checkmate, leaving or resigning
-
-server can interrupt client actions -
-things like help and show moves may be interrupted by the other client through the server
-
-client can also do things like help, redraw board, leave, make move, resign, and highlight legal moves
-not all of these require interaction with the server
-
-
- */
